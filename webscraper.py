018240bc1f655a066065a961c5e56e42535c2539
DanTheDev27 <153468216+DanTheDev27@users.noreply.github.com>
2024-05-08 12:01:21 -0400

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -2,22 +2,33 @@ import requests
 import urllib
 import json
 import os.path
+import csv
 
-
-
+#url to scrape from
 request_url = requests.get('https://microsoftedge.github.io/Demos/json-dummy-data/64KB-min.json')
-#data = request_url.json()
 
 #pathtofile
-path = "C:/Users/Danny/Documents/SDevSpring24/WorkStudyProjects"
+path = "D:/Users/soitguest/Documents"
+#write data to file in designated location
 fileLocation = os.path.join(path,'testFile'+'.json')
 """ with open(fileLocation,'w') as file:
     json.dump(data,file) """
-params = 'name'
-with open(fileLocation,'r') as file:
-    data= json.load(file)
-formattedData= json.dumps(data[0], indent=4)
-print(formattedData)
 
+#opening and parsing data by parameters
+file = open(fileLocation)
+jsonLoad = json.load(file)
+formattedFile = json.dumps(jsonLoad, indent=4)
+#print(formattedFile)
+for item in jsonLoad:
+    names = item['name']
+    id = item['id']
+    nameId = "Full name: " + names + "\n id: " + id
+    print(nameId)
 
-
+#csv writer
+""" fields = []
+with open('parsedData.csv', 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames= fields)
+    writer.writeheader()
+    writer.writerows()
+ """
