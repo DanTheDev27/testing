f97b7d30960faa24017c9438e95b2bef65319cbe
DanTheDev27 <153468216+DanTheDev27@users.noreply.github.com>
2024-05-13 17:09:25 -0400

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -7,15 +7,33 @@ import csv
 #url to scrape from
 request_url = requests.get('https://microsoftedge.github.io/Demos/json-dummy-data/64KB-min.json')
 
-#pathtofile
+with urllib.request.urlopen('https://microsoftedge.github.io/Demos/json-dummy-data/64KB-min.json') as url:
+    data = json.load(url)
+    #print(data)
+
+for item in data:
+    names = item['name']
+    id = item['id']
+    language = item['language']
+    print(f"Full name: {names} \n Id: {id} \n {language}")
+
+
+
+
+
+
+
+#code to save and write file
+
+#Path where the file will be saved
 path = ""
 #write data to file in designated location
-fileLocation = os.path.join(path,'testFile'+'.json')
-""" with open(fileLocation,'w') as file:
+""" fileLocation = os.path.join(path,'testFile'+'.json')
+with open(fileLocation,'w') as file:
     json.dump(data,file) """
 
 #opening and parsing data by parameters
-file = open(fileLocation)
+""" file = open(fileLocation)
 jsonLoad = json.load(file)
 formattedFile = json.dumps(jsonLoad, indent=4)
 #print(formattedFile)
@@ -23,12 +41,4 @@ for item in jsonLoad:
     names = item['name']
     id = item['id']
     nameId = "Full name: " + names + "\n id: " + id
-    print(nameId)
-
-#csv writer
-""" fields = []
-with open('parsedData.csv', 'w') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames= fields)
-    writer.writeheader()
-    writer.writerows()
- """
+    print(nameId) """
