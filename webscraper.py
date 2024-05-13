9765fe83b28c5de3d52f508cc7d4e46a1ac5c7d8
DanTheDev27 <153468216+DanTheDev27@users.noreply.github.com>
2024-05-13 18:09:17 -0400

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -2,7 +2,7 @@ import requests
 import urllib
 import json
 import os.path
-import csv
+
 
 #url to scrape from
 request_url = requests.get('https://microsoftedge.github.io/Demos/json-dummy-data/64KB-min.json')
@@ -15,7 +15,7 @@ for item in data:
     names = item['name']
     id = item['id']
     language = item['language']
-    print(f"Full name: {names} \n Id: {id} \n {language}")
+    print(f"Full name : {names} \n Id : {id} \n language : {language}")
 
 
 
@@ -26,19 +26,8 @@ for item in data:
 #code to save and write file
 
 #Path where the file will be saved
-path = ""
+#path = ""
 #write data to file in designated location
 """ fileLocation = os.path.join(path,'testFile'+'.json')
 with open(fileLocation,'w') as file:
     json.dump(data,file) """
-
-#opening and parsing data by parameters
-""" file = open(fileLocation)
-jsonLoad = json.load(file)
-formattedFile = json.dumps(jsonLoad, indent=4)
-#print(formattedFile)
-for item in jsonLoad:
-    names = item['name']
-    id = item['id']
-    nameId = "Full name: " + names + "\n id: " + id
-    print(nameId) """
